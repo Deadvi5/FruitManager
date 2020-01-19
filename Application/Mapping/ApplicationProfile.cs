2544edee6854f4d79c00da4bda016ade1f6f5d57
Lorenzo Villa <lorenzo.villa.larry@gmail.com>
2020-01-19 23:08:40 +0100

    remove using

--- Application/Mapping/ApplicationProfile.cs
+++ Application/Mapping/ApplicationProfile.cs
@@ -1,9 +1,6 @@
 ﻿using AutoMapper;
 using FruitManager.Application.Abstraction.Model;
 using FruitManager.DataAccessLayer.Abstraction.Model;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace FruitManager.Application.Mapping
 {
